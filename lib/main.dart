0828d433cbf879d817aea915ce876ffa99408ceb
Aeeshan Ahmad <zeeshanahmad941983@gmail.com>
2024-06-15 01:13:42 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() {
   runApp(const MyApp());
@@ -51,7 +52,32 @@ class _MyHomePageState extends State<MyHomePage> {
           planeDetection: ARPlaneDetection.horizontalAndVertical,
           onARKitViewCreated: (ARKitController controller) {
             this.controller = controller;
+            controller.onARTap = (hit) {
+              final point = hit.firstWhere(
+                  (hit) => hit.type == ARKitHitTestResultType.featurePoint);
+              if (point != null) {
+                _onArtapHandler(point);
+              }
+            };
           },
         ));
+  }
+
+  void _onArtapHandler(ARKitTestResult point) {
+    final position = vector.Vector3(
+      point.worldTransform.getColumn(3).x,
+      point.worldTransform.getColumn(3).y,
+      point.worldTransform.getColumn(3).z,
+    );
+    final node = _getNodeFromFlutterAssets(position);
+    controller.add(node);
+  }
+
+  ARKitGltfNode _getNodeFromFlutterAssets(vector.Vector3 position) {
+    return ARKitGltfNode(
+        assetType: AssetType.flutterAsset,
+        url: 'assets/3d_weird_bubble.gltf',
+        scale: vector.Vector3.all(0.5),
+        position: position);
   }
 }
